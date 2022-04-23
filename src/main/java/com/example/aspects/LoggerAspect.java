2653a92c00ddcb9d832cb4ec691cf4ab13cbf7c5
nischal <nischal.bhatt.87@gmail.com>
2022-04-23 09:07:03 +0800

    order

--- src/main/java/com/example/aspects/LoggerAspect.java
+++ src/main/java/com/example/aspects/LoggerAspect.java
@@ -1,16 +1,17 @@
 package com.example.aspects;
 
-import java.time.Instant;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 
 @Component
+@Order(1)
 public class LoggerAspect {
 
 	 private Logger logger = Logger.getLogger(LoggerAspect.class.getName());
